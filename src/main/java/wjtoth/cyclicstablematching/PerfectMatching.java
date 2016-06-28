c5d3e7001a5bfe59e6aa0cb9c06044dcc16aa4a6
William Justin Toth <wjtoth@live.com>
2016-06-27 23:13:15 -0400

    Update PerfectMatching.java

--- src/main/java/wjtoth/cyclicstablematching/PerfectMatching.java
+++ src/main/java/wjtoth/cyclicstablematching/PerfectMatching.java
@@ -5,8 +5,20 @@ public class PerfectMatching {
 	private final int NUMBER_OF_GROUPS;
 	private final int NUMBER_OF_AGENTS;
 	
+	//Store matching
+	
 	public PerfectMatching(int numberOfGroups, int numberOfAgents) {
 		NUMBER_OF_GROUPS = numberOfGroups;
 		NUMBER_OF_AGENTS = numberOfAgents;
 	}
+	
+	//set matching from ArrayList<int[]> of size numberOfGroups with numberOfAgents sized permutations
+	
+	//set matching directly from triples
+	
+	//get match of agent indexed j in group i
+	
+	//get triple of agent indexed j in group i
+	
+	//get all triples
 }
