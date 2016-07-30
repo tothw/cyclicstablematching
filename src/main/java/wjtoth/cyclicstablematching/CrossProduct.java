853d9ff57de06e57602bd9649a0843bd38e2e10f
justin <tothw@uwindsor.ca>
2016-07-30 19:10:49 -0400

    Starting to implement claim 1

--- src/main/java/wjtoth/cyclicstablematching/CrossProduct.java
+++ src/main/java/wjtoth/cyclicstablematching/CrossProduct.java
@@ -1,18 +1,31 @@
 package wjtoth.cyclicstablematching;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class CrossProduct<T> {
 	
-	private ArrayList<T> set;
+	private ArrayList<ArrayList<T>> sets;
 	private int dimension;
-
 	private int[] indices;
 	private boolean hasNext;
-	
+
+
 	public CrossProduct(ArrayList<T> set, int dimension) {
-		this.set = set;
-		this.dimension = dimension;
+		ArrayList<ArrayList<T>> sets = new ArrayList<>(dimension);
+		for(int i = 0; i<dimension; ++i) {
+			sets.add(set);
+		}
+		initialize(sets);
+	}
+
+	public CrossProduct(ArrayList<ArrayList<T>> sets) {
+		initialize(sets);
+	}
+
+	private void initialize(ArrayList<ArrayList<T>> sets) {
+		this.sets = sets;
+		this.dimension = sets.size();
 		indices = new int[dimension];
 		hasNext = true;
 	}
@@ -24,7 +37,7 @@ public class CrossProduct<T> {
 	public ArrayList<T> next() {
 		ArrayList<T> retval = new ArrayList<T>();
 		for(int i = 0; i<dimension; ++i) {
-			retval.add(set.get(indices[i]));
+			retval.add(sets.get(i).get(indices[i]));
 		}
 		incrementIndices();
 		return retval;
@@ -34,6 +47,7 @@ public class CrossProduct<T> {
 		int index = indices.length-1;
 		boolean successFlag = false;
 		while(index >= 0) {
+			ArrayList<T> set = sets.get(index);
 			indices[index] = (indices[index] + 1) % set.size();
 			if(indices[index] == 0) {
 				--index;
